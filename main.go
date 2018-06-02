022f66ed8e8874eac829fc122e369a6f06b490ef
Porawit Poboonma <ball6847@gmail.com>
2018-06-02 11:50:19 +0700

    fix: from package renaming

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	"github.com/ball6847/hello/object"
-	"github.com/ball6847/hello/subject"
-	"github.com/ball6847/hello/verb"
+	"github.com/ball6847/go-hello/object"
+	"github.com/ball6847/go-hello/subject"
+	"github.com/ball6847/go-hello/verb"
 )
 
 func main() {
